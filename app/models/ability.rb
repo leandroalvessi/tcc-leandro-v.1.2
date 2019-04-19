b451d5c269b799acd98a8594c9cfc2984f4cb055
Leandro Alves <leandroalves.si@hotmail.com>
2019-04-19 00:31:37 -0300

    #13 - Permissão rails admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,20 +1,19 @@
 class Ability
   include CanCan::Ability
 
+  #can [:create, :read, :update, :destroy], Balance
   def initialize(user)
     if user.kind == 'Gerente' && user.status == 'Ativo'      
       can :manage, :all
+      can [:create, :read], Product, user_id: user.id
     elsif user.kind == 'Gerente' && user.status == 'Inativo'
       can :dashboard, :all
       can :access, :rails_admin 
-      #can [:create, :read, :update, :destroy], Balance
       can :read, :all
     elsif user.kind == 'Usuário' && user.status == 'Ativo'
       can :dashboard, :all
       can :access, :rails_admin
-      #can [:create, :read, :update, :destroy], Balance
       can :read, :all
-      can [:create, :read], Product, user_id: user.id
     else
       can :dashboard, :all
       can :access, :rails_admin
